67dbe95b779260ad3c9eeb726ae74b3db17bc35b
Lucas Oliveira <olivmath@protonmail.com>
2025-06-04 21:19:25 -0300

    update backend

--- indexer - backend/src/blockchain.js
+++ indexer - backend/src/blockchain.js
@@ -1,10 +1,10 @@
-const { createPublicClient, http, parseAbiItem } = require('viem');
-const { anvil } = require('viem/chains');
+const { createPublicClient, http, parseAbiItem, formatEther } = require('viem');
+const { sepolia } = require('viem/chains');
 const { db } = require('./database');
 
 // Configuração do cliente blockchain
 const client = createPublicClient({
-  chain: anvil,
+  chain: sepolia,
   transport: http()
 });
 
@@ -18,7 +18,7 @@ function normalizeTaskData(event) {
     id: Number(event.args.id),
     title: event.args.title,
     description: event.args.description,
-    stake: event.args.stake.toString(),
+    stake: formatEther(event.args.stake.toString()),
     created_at: Number(event.args.createdAt),
     completed_at: Number(event.args.completedAt),
     due_date: Number(event.args.dueDate),
@@ -27,10 +27,34 @@ function normalizeTaskData(event) {
   };
 }
 
+async function fetchPastEvents() {
+  // TODO: Substituir pelo endereço real do contrato
+  const contractAddress = '0x034FAeae891F47a2714eb1e4BfbA7525a606DcC5';
+
+  const last1000block = await client.getBlockNumber() - BigInt(1000);
+  console.log(last1000block)
+
+  // Busca todos os logs do evento TaskCreated desde o bloco 0 até o mais recente
+  const logs = await client.getLogs({
+    address: contractAddress,
+    event: taskCreatedEvent,
+    fromBlock: last1000block,
+    toBlock: 'latest'
+  });
+
+  for (const log of logs) {
+    const taskData = normalizeTaskData(log);
+    await db.saveTask(taskData);
+    console.log('✅ Task salva (histórico):', taskData);
+  }
+}
+
 async function setupBlockchainListener() {
   try {
+    await fetchPastEvents();
+
     // TODO: Substituir pelo endereço real do contrato
-    const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+    const contractAddress = '0x034FAeae891F47a2714eb1e4BfbA7525a606DcC5'
     
     // Configura o listener de eventos
     client.watchEvent({
